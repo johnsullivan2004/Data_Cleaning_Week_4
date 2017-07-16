a212836ef959aa6e12c38bcdbe6a36f4527f1aa0
johnsullivan2004 <sat7e7b@yahoo.com>
2017-07-15 20:44:47 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
-# Data_Cleaning_Week_4
-Week 4 Assignment for Coursera Data Cleaning Class by John Hopkins
+# Data Cleaning Assignment for week #4
+## By John Sullivan
+
+This is the week #4 Assignment for the Coursera Data Concentration Data Cleaning class.
+
+I've use the Human Activity Recognition data Using Smartphones as described by this website:
+<http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones>
+
+The input dataset is found here:
+<https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip>
+
+My script is found in the *run_analysis.R* file in this github section.
+
+The output dataset is included as well - *HAR_tidy.txt*.
+
+##  Codebook for HAR_tidy.txt
+
+Each Row is one observation as per tidy dataset requirements.
+* Fields that define the observation:
+	+ Test_SubjectID - One of the 30 anonymous subjects
+	+ ActivityID - Activity ID
+	+ ActivityLabel - Corresponding activity type lable
+	+ TestTrain - Either Train or Test - indicates which data set
+* Next are the derived mean and standard deviations from the raw data.   These are the mean and standard deviations as derived from the 128 measurements for the x/y/z accelerometer, gyroscope, and total.   The script calculates these from the raw Inertial Signal data files.  There are fields for each measurement type, axis, and mean or standard deviation.
+	+ HAR_body_acc_x_mean
+	+ HAR_body_acc_x_std
+	+ HAR_body_acc_y_mean
+	+ HAR_body_acc_y_std
+	+ HAR_body_acc_z_mean
+	+ HAR_body_acc_z_std
+	+ HAR_body_gyro_x_mean
+	+ HAR_body_gyro_x_std
+	+ HAR_body_gyro_y_mean
+	+ HAR_body_gyro_y_std
+	+ HAR_body_gyro_z_mean
+	+ HAR_body_gyro_z_std
+	+ HAR_total_acc_x_mean
+	+ HAR_total_acc_x_std
+	+ HAR_total_acc_y_mean
+	+ HAR_total_acc_y_std
+	+ HAR_total_acc_z_mean
+	+ HAR_total_acc_z_std
+* Then, are these variables.  These come from the HAR_X dataset and were derived from extensive processing by the HAR project team.   These are a subset of the HAR_X data - only the means and standard deviations are retained.  See the HAR features_info.txt file for more information.
+	+ tBodyAcc-mean()-X
+	+ tBodyAcc-mean()-Y
+	+ tBodyAcc-mean()-Z
+	+ tBodyAcc-std()-X
+	+ tBodyAcc-std()-Y
+	+ tBodyAcc-std()-Z
+	+ tGravityAcc-mean()-X
+	+ tGravityAcc-mean()-Y
+	+ tGravityAcc-mean()-Z
+	+ tGravityAcc-std()-X
+	+ tGravityAcc-std()-Y
+	+ tGravityAcc-std()-Z
+	+ tBodyAccJerk-mean()-X
+	+ tBodyAccJerk-mean()-Y
+	+ tBodyAccJerk-mean()-Z
+	+ tBodyAccJerk-std()-X
+	+ tBodyAccJerk-std()-Y
+	+ tBodyAccJerk-std()-Z
+	+ tBodyGyro-mean()-X
+	+ tBodyGyro-mean()-Y
+	+ tBodyGyro-mean()-Z
+	+ tBodyGyro-std()-X
+	+ tBodyGyro-std()-Y
+	+ tBodyGyro-std()-Z
+	+ tBodyGyroJerk-mean()-X
+	+ tBodyGyroJerk-mean()-Y
+	+ tBodyGyroJerk-mean()-Z
+	+ tBodyGyroJerk-std()-X
+	+ tBodyGyroJerk-std()-Y
+	+ tBodyGyroJerk-std()-Z
+	+ tBodyAccMag-mean()
+	+ tBodyAccMag-std()
+	+ tGravityAccMag-mean()
+	+ tGravityAccMag-std()
+	+ tBodyAccJerkMag-mean()
+	+ tBodyAccJerkMag-std()
+	+ tBodyGyroMag-mean()
+	+ tBodyGyroMag-std()
+	+ tBodyGyroJerkMag-mean()
+	+ tBodyGyroJerkMag-std()
+	+ fBodyAcc-mean()-X
+	+ fBodyAcc-mean()-Y
+	+ fBodyAcc-mean()-Z
+	+ fBodyAcc-std()-X
+	+ fBodyAcc-std()-Y
+	+ fBodyAcc-std()-Z
+	+ fBodyAccJerk-mean()-X
+	+ fBodyAccJerk-mean()-Y
+	+ fBodyAccJerk-mean()-Z
+	+ fBodyAccJerk-std()-X
+	+ fBodyAccJerk-std()-Y
+	+ fBodyAccJerk-std()-Z
+	+ fBodyGyro-mean()-X
+	+ fBodyGyro-mean()-Y
+	+ fBodyGyro-mean()-Z
+	+ fBodyGyro-std()-X
+	+ fBodyGyro-std()-Y
+	+ fBodyGyro-std()-Z
+	+ fBodyAccMag-mean()
+	+ fBodyAccMag-std()
+	+ fBodyBodyAccJerkMag-mean()
+	+ fBodyBodyAccJerkMag-std()
+	+ fBodyBodyGyroMag-mean()
+	+ fBodyBodyGyroMag-std()
+	+ fBodyBodyGyroJerkMag-mean()
+	+ fBodyBodyGyroJerkMag-std()
+
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,91 @@
+# Peer-graded Assignment: Getting and Cleaning Data Course Project
+#
+# John Sullivan - 7/8/2017
+# Step 0 - My favorite libaries
+#
+
+
+library(dplyr)
+library(tidyr)
+library(data.table)
+library(lubridate)
+
+#
+# Step 1 - Get the Data
+
+if (dir.exists("./data")) {cat ("Note: ./data directory present in working directory \n")
+} else {dir.create("./data")
+        cat("./data directory not present; creating ./data it in working directory \n")}
+
+zip_file_name <- "./data/HAR_Dataset.zip"
+if (!file.exists(zip_file_name))  {
+    download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+                  , dest=zip_file_name, mode = "wb") 
+        unzip (zipfile=zip_file_name, exdir = "./data")
+}
+
+# Step 2 - Read the data tables
+
+# function to read entire directories into data.tables
+# Notes: I coded this myself as an exercise without consulting the web.
+#
+read_whole_dir <- function(data_dir,read_data_files = NULL) {
+    cat ("Reading from directory ",data_dir,"\n")
+    # Get the files that end in .txt or .csv unless they are already specified
+    if (is.null(read_data_files))  {read_data_files <- grep(".txt$|.cvs$",dir(data_dir,include.dirs = FALSE),value=TRUE)}
+    # Start the loop to read the files
+    for (i in seq_along(read_data_files)) {
+        cat(paste(paste0(data_dir,"/",read_data_files[i])," --> ",gsub(".txt","",read_data_files[i])),"\n")
+        assign(gsub(".txt","",read_data_files[i]),fread(paste0(data_dir,"/",read_data_files[i]),header=FALSE),envir = .GlobalEnv)
+    }
+}
+
+read_whole_dir("./data/UCI HAR Dataset/test/Inertial Signals")
+read_whole_dir("./data/UCI HAR Dataset/train/Inertial Signals")
+read_whole_dir("./data/UCI HAR Dataset/test")
+read_whole_dir("./data/UCI HAR Dataset/train")
+read_whole_dir("./data/UCI HAR Dataset",c("features.txt","activity_labels.txt"))
+
+# Step 3 - Merge the training and test datasets
+cat("Merging data sets. \n")
+HAR_datasets <- gsub("_train$","",grep("_train$",ls(),value=TRUE))
+for (i in seq_along(HAR_datasets)) {
+    cat(paste0("HAR_",HAR_datasets[i]),"<- ",paste0(HAR_datasets[i],"_train")," + ",paste0(HAR_datasets[i],"_test"),"\n")
+    assign(paste0("HAR_",HAR_datasets[i]),bind_rows(get(paste0(HAR_datasets[i],"_train")), get(paste0(HAR_datasets[i],"_test"))))
+}
+
+# Step 4 - Create Means and Std for granular data sets
+cat("\n Creating Means and Std for granular data sets \n")
+HAR_granular_ds <- grep("^HAR_body|^HAR_total",ls(),value=TRUE)
+HAR_granular_mstd <- data.table("obs_num" = 1:nrow(get(HAR_granular_ds[1])))
+for (i in seq_along(HAR_granular_ds)) {
+    cat(paste0("Extracting mean() and std() from ",HAR_granular_ds[i]," and adding it to HAR_granular_mstd \n"))
+    HAR_granular_mstd <- cbind(HAR_granular_mstd,
+        select (
+            cbind("obs_num" = 1:nrow(get(HAR_granular_ds[i])), get(HAR_granular_ds[i])) %>%
+            gather(datac,result,V1:V128) %>%
+            group_by(obs_num) %>%
+            summarize(mn=mean(result),std=sd(result)) %>%
+            rename_(.dots=setNames(c("mn","std"), c(paste0(HAR_granular_ds[i],"_mean"),paste0(HAR_granular_ds[i],"_std"))))
+        ,-obs_num)
+    )
+}
+
+# Step 5 - Assign missing label names
+setnames(HAR_X,names(HAR_X),features$V2)
+setnames(HAR_y,"V1","ActivityID")
+setnames(HAR_subject,"V1","Test_SubjectID")
+setnames(activity_labels,c("V1","V2"),c("ActivityID","ActivityLabel"))
+
+# Step 6 - create the tidy dataset by cbinding the component datasets
+HAR_X_keep <- grep("mean\\(\\)|std\\(\\)",names(HAR_X),value=TRUE) #Columns to Keep
+HAR_tidy <- cbind(
+        HAR_subject,
+        left_join(HAR_y, activity_labels, by = "ActivityID"),
+        transmute(HAR_granular_mstd, TestTrain = as.factor(ifelse(obs_num <= nrow(subject_train),"Train","Test"))),
+        select(HAR_granular_mstd,-obs_num),
+        HAR_X[,..HAR_X_keep]
+        )
+
+# Step 7 - Write out the tidy dataset
+write.table(HAR_tidy,"./data/HAR_tidy.txt",row.name=FALSE)
